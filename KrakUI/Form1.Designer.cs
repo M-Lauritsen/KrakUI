589d0f019e574744f9cf90cd1d9d844221ced2f7
Michael Lauritsen <i30288741@live.dk>
2018-11-21 14:26:15 +0100

    bare for sjov... den virker... sådan da.. ;)

--- KrakUI/Form1.Designer.cs
+++ KrakUI/Form1.Designer.cs
@@ -28,13 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Søg";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(93, 59);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(589, 439);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(93, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(163, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 523);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- KrakUI/Form1.cs
+++ KrakUI/Form1.cs
@@ -5,8 +5,12 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using HtmlAgilityPack;
+using System.Net.Http;
+using HtmlDocument = HtmlAgilityPack.HtmlDocument;
 
 namespace KrakUI
 {
@@ -15,6 +19,82 @@ namespace KrakUI
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread t = new Thread(() => Start());
+            t.Start();
+        }
+
+        async void Start()
+        {
+            string q = textBox1.Text;
+
+            var url = "https://www.krak.dk/" + q + "/personer";
+
+            try
+            {
+                // Using the http client with Async
+                var httpClient = new HttpClient();
+                var html = await httpClient.GetStringAsync(url);
+
+                // Load the html document
+                var HtmlDocument = new HtmlDocument();
+                HtmlDocument.LoadHtml(html);
+
+                // a list of every result
+                var NameList = HtmlDocument.DocumentNode.Descendants("ul")
+                    .Where(node => node.GetAttributeValue("class", "")
+                    .Equals("ResultList")).ToList();
+
+                // Single person added to list
+                var NameListPeople = NameList[0].Descendants("div")
+                    .Where(node => node.GetAttributeValue("class", "")
+                    .Equals("PersonResultListItem")).ToList();
+
+                foreach (HtmlNode person in NameListPeople)
+                {
+                    try
+                    {
+                        Invoke(new MethodInvoker(delegate
+                        {
+                            richTextBox1.Text = richTextBox1.Text + person.Descendants("div")
+                       .Where(node => node.GetAttributeValue("class", "")
+                       .Equals("personName")).FirstOrDefault().InnerText.Trim('\r', '\n', '\t');
+
+                            // Address
+                            richTextBox1.Text = richTextBox1.Text + person.Descendants("div")
+                            .Where(node => node.GetAttributeValue("class", "")
+                            .Equals("address")).FirstOrDefault().InnerText.Trim('\r', '\n', '\t');
+
+                            // PhoneNumber
+                            richTextBox1.Text = richTextBox1.Text + person.Descendants("div")
+                            .Where(node => node.GetAttributeValue("role", "")
+                            .Equals("button")).FirstOrDefault().InnerText.Trim('\r', '\n', '\t').Substring(0, 11) +Environment.NewLine;
+                        }));
+                    }
+                    catch (Exception ex)
+                    {
+                        Invoke(new MethodInvoker(delegate
+                        {
+                            richTextBox1.Text = richTextBox1.Text + " Skjult" + Environment.NewLine;
+                            
+                        }));
+                    }
+
+
+                };
+            }
+            catch (Exception)
+            {
+
+                richTextBox1.Text = richTextBox1.Text + "blank" + Environment.NewLine;
+            }
+
+            int count = 0;
+
+            
         }
     }
 }
